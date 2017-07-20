1623a0c8b92077393bf225d50e468ceba9eceecb
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-20 13:20:13 -0700

    Dogs year with Js

---
+++ pupyAgeCalculator.js
@@ -0,0 +1,8 @@
+// 1 human year to 7 dog years.
+function calculateDogAge(pupyAge) {
+	var dogYears = (pupyAge * 7);
+	console.log("Your doggie is " + dogYears + " years old in dog years!");
+}
+calculateDogAge(1);
+calculateDogAge(4);
+calculateDogAge(8);
